1b7d4b0469b5b0816e5a0ba0278d938cc869b30e
eselectron <2727262253@qq.com>
2018-02-12 21:34:07 +0800

    Create it

---
+++ app/src/main/java/edu/zhku/jsj144/lzc/video/MainActivity.java
@@ -0,0 +1,86 @@
+package edu.zhku.jsj144.lzc.video;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import edu.zhku.jsj144.lzc.video.fragment.DiscoverPageFragment;
+import edu.zhku.jsj144.lzc.video.fragment.FollowPageFragment;
+import edu.zhku.jsj144.lzc.video.fragment.MinePageFragment;
+import edu.zhku.jsj144.lzc.video.viewpager.NoScrollViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private NoScrollViewPager mPager;
+    private PagerAdapter mPagerAdapter;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_notifications:
+                    mPager.setCurrentItem(2);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (NoScrollViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+        // Instantiate a BottomNavigationView
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0:
+                    fragment =  new DiscoverPageFragment();
+                    break;
+                case 1:
+                    fragment = new FollowPageFragment();
+                    break;
+                case 2:
+                    fragment = new MinePageFragment();
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/zhku/jsj144/lzc/video/fragment/FollowPageFragment.java
@@ -0,0 +1,20 @@
+package edu.zhku.jsj144.lzc.video.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import edu.zhku.jsj144.lzc.video.R;
+
+public class FollowPageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.fragment_page_follow, container, false);
+
+        return rootView;
+    }
+}
