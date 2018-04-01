7a4b28ccbfcd43a23e7ea6489e3b831c013b5ba0
eselectron <2727262253@qq.com>
2018-04-01 16:59:13 +0800

    Add search module

--- app/src/main/java/edu/zhku/jsj144/lzc/video/fragment/DiscoverPageFragment.java
+++ app/src/main/java/edu/zhku/jsj144/lzc/video/fragment/DiscoverPageFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.SearchView;
 import edu.zhku.jsj144.lzc.video.R;
 import edu.zhku.jsj144.lzc.video.application.BaseApplication;
 import edu.zhku.jsj144.lzc.video.util.WebUtil;
@@ -25,6 +26,24 @@ public class DiscoverPageFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_page_discover, container, false);
+
+        SearchView searchView = rootView.findViewById(R.id.search1);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() == 0) {
+                    webView.loadUrl(BaseApplication.PAGE_BASE_URL + "/discover");
+                } else {
+                    webView.loadUrl(BaseApplication.PAGE_BASE_URL + "/searchedVideos?title=" + newText);
+                }
+                return true;
+            }
+        });
 
         webView = (WebView) rootView.findViewById(R.id.discoverWebview);
         webView.addJavascriptInterface(new WebUtil(activity), "android");
--- app/src/main/java/edu/zhku/jsj144/lzc/video/fragment/SubscribePageFragment.java
+++ app/src/main/java/edu/zhku/jsj144/lzc/video/fragment/SubscribePageFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.widget.SearchView;
 import edu.zhku.jsj144.lzc.video.R;
 import edu.zhku.jsj144.lzc.video.application.BaseApplication;
 import edu.zhku.jsj144.lzc.video.util.WebUtil;
@@ -25,6 +26,24 @@ public class SubscribePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_page_subscribe, container, false);
+
+        SearchView searchView = rootView.findViewById(R.id.search2);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if (newText.length() == 0) {
+                    webView.loadUrl(BaseApplication.PAGE_BASE_URL + "/subscribe?r=all");
+                } else {
+                    webView.loadUrl(BaseApplication.PAGE_BASE_URL + "/searchedUsers?username=" + newText);
+                }
+                return true;
+            }
+        });
 
         webView = (WebView) rootView.findViewById(R.id.subscribeWebview);
         webView.addJavascriptInterface(new WebUtil(activity), "android");
